998228cdc7469ed4d950de5a79f39dbe70d59914
robinr10122002-spec <robinr10122002@gmail.com>
2025-12-19 18:07:23 +0530

    FUNCTIONS

---
+++ Day-7/functions.js
@@ -0,0 +1,29 @@
+// function moodReport(emojie="😊",timeOfTheDay="Evening"){
+//     const climateToday=`Feeling${emojie},this is ${timeOfTheDay}`
+//     return climateToday;
+// }
+//   console.log(moodReport());
+
+// console.log(moodReport("😎", "afternoon ☀️"));
+
+// function moodReport(emojie1="😊",timeOfTheDay="evening"){                   task 1
+//     const climateToday=`Feelings${emojie1},this is ${timeOfTheDay}`
+//     return climateToday;
+// }
+// console.log(moodReport());
+// console.log(moodReport("😎","morning"));
+
+// task 1.2
+// function checkAgeCategory(age = 18) {
+//   if (age >= 60) {
+//     return `senior`;
+//   } else if (age < 18) {
+//     return `minor`;
+//   }
+//   return `adult`;
+// }
+// console.log(checkAgeCategory());
+// console.log(checkAgeCategory(12));
+// console.log(checkAgeCategory(65));
+
+// task 1.3
