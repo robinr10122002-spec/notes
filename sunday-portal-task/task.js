f827f938ea3514b126cc7b81ba16fee54e941779
robinr10122002-spec <robinr10122002@gmail.com>
2026-01-04 21:04:31 +0530

    capitalize task

--- sunday-portal-task/task.js
+++ sunday-portal-task/task.js
@@ -6,6 +6,10 @@ const toTitleCase = (str) => {
   return str
   .split(" ")
   .map(word=>word[0].toUpperCase()+word.slice(1)).join(" ")
+
+//   return str
+//   .split(" ")
+//   .map(word=>word[0].toUpperCase()+word.slice(1)).join(" ")
   
   
   
