e23ba275625fdad422f5e440de4512c762e70e78
robinr10122002-spec <robinr10122002@gmail.com>
2026-01-04 21:23:08 +0530

    playlist-11-task-2

--- sunday-portal-task/task.js
+++ sunday-portal-task/task.js
@@ -1,21 +1,77 @@
-const sentence = 'mastering javascript is fun';
+                         //capitalize the first letter task in portal
 
-// Write a function to convert sentence to title case
-const toTitleCase = (str) => {
-  // Your code here
-  return str
-  .split(" ")
-  .map(word=>word[0].toUpperCase()+word.slice(1)).join(" ")
+//const sentence = 'mastering javascript is fun';
 
+// // Write a function to convert sentence to title case
+// const toTitleCase = (str) => {
+//   // Your code here
 //   return str
 //   .split(" ")
 //   .map(word=>word[0].toUpperCase()+word.slice(1)).join(" ")
-  
-  
-  
 
-}
+// }
 
-console.log(toTitleCase(sentence));
+// console.log(toTitleCase(sentence));
+
+
+
+// playlist-11--> 2nd task
+
+const movies = [
+  {
+    id: 1,
+    title: "Baahubali",
+    director: "S. S. Rajamouli",
+    year: 2015,
+    ratings: [8, 9, 10],
+    genre: "Action",
+  },
+  {
+    id: 2,
+    title: "Arjun Reddy",
+    director: "Sandeep Reddy Vanga",
+    year: 2017,
+    ratings: [9, 8, 9],
+    genre: "Drama",
+  },
+  {
+    id: 3,
+    title: "Mahanati",
+    director: "Nag Ashwin",
+    year: 2018,
+    ratings: [10, 9, 8],
+    genre: "Biography",
+  },
+  {
+    id: 4,
+    title: "Eega",
+    director: "S. S. Rajamouli",
+    year: 2012,
+    ratings: [7, 8, 9],
+    genre: "Fantasy",
+  },
+  {
+    id: 5,
+    title: "Jersey",
+    director: "Gowtam Tinnanuri",
+    year: 2019,
+    ratings: [9, 9, 8],
+    genre: "Sports",
+  },
+];
+
+// Write a function that checks if all movies of a genre have high ratings
+const allRatingsAboveForGenre = (movies, rating, genre) => {
+const movieGenre=movies.filter(movie=>movie.genre==genre)
+if(movieGenre.length==0){return`no movies present in the specific genre`}
+const highestRating=movieGenre.every(movie=>movie.ratings.every(r=>r>rating))
+return highestRating?`yes${genre}movie has ${rating}ratings`:`no${genre}has no ${rating} rating`
+ 
+
+};
+
+console.log(allRatingsAboveForGenre(movies, 7, "Action"));
+console.log(allRatingsAboveForGenre(movies, 8, "Biography"));
+
 
  
