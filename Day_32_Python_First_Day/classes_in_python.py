0d77896d6fffcad6233de90065464296e6daec9d
robinr10122002-spec <robinr10122002@gmail.com>
2026-02-24 22:11:52 +0530

    class

--- Day_32_Python_First_Day/classes_in_python.py
+++ Day_32_Python_First_Day/classes_in_python.py
@@ -1,15 +1,16 @@
 class Car:
-    def __init__(self,wheels,doors,model,engine):
-     self.wheels=wheels
-     self.doors=doors
-     self.model=model
-     self.engine=engine
+    def __init__(self, wheels, doors, model, engine):
+        self.wheels = wheels
+        self.doors = doors
+        self.model = model
+        self.engine = engine
 
-Lamborgini=Car(4,2,"Huracan","v16")
+
+Lamborgini = Car(4, 2, "Huracan", "v16")
 
 print(Lamborgini)
 print(Lamborgini.model)
-Hindustan=Car(4,4,"Ambassador","v4")
+Hindustan = Car(4, 4, "Ambassador", "v4")
 print(Hindustan)
 print(Hindustan.wheels)
 
@@ -19,21 +20,21 @@ print(Hindustan.wheels)
 # - Engine - v4
 # - Model - Harrier
 # - Doors - 4
-Tata=Car(4,4,"Harrier","v4")
+Tata = Car(4, 4, "Harrier", "v4")
 print(Tata)
 print(Tata.model)
 
 # .......................................................................................................
 
 
-
 class Passbook:
-   def __init__(self,account_number,ifsc_code,branch,holder_name):
-    self.account_number=account_number
-    self.ifsc_code=ifsc_code
-    self.branch=branch
-    self.holder_name=holder_name
+    def __init__(self, account_number, ifsc_code, branch, holder_name):
+        self.account_number = account_number
+        self.ifsc_code = ifsc_code
+        self.branch = branch
+        self.holder_name = holder_name
 
-IOB=Passbook(129304,"IOBA0032","Chennai","Robin")
+
+IOB = Passbook(129304, "IOBA0032", "Chennai", "Robin")
 print(IOB)
 print(IOB.holder_name)
