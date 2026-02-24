906881cfdba5e6e8b428a2ae37bacfa350f1d090
robinr10122002-spec <robinr10122002@gmail.com>
2026-02-24 10:06:08 +0530

    message_task_completed

--- Day_32_Python_First_Day/string-methods.py
+++ Day_32_Python_First_Day/string-methods.py
@@ -9,11 +9,24 @@
 
 
 # Task 1.2
-flipped_message = "!nuf sseldnE dna seitinutroppo lufrewop htiw nohtyP"
-original_message = flipped_message[::-1]
-print(original_message)
-print(f"{original_message[:6]} 🗡️   {original_message[12:20]} 🌸".lower())
+# flipped_message = "!nuf sseldnE dna seitinutroppo lufrewop htiw nohtyP"
+# original_message = flipped_message[::-1]
+# print(original_message)
+# print(f"{original_message[:6]} 🗡️   {original_message[12:20]} 🌸".lower())
 
 
 # Expected Output
 # "python 🗡️ powerful 🌸"
+
+# ...................................................................................
+
+# Task 1.3 (Home Assignment)
+
+# After the 🔑
+message = "    🚨🔍📱🔑secret_code✌️".strip().upper()
+
+print(message[4:])
+# Clue: find
+
+# Output
+# SECRET_CODE✌️
